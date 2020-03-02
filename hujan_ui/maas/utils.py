7eca40f04d4407732988ac1c0b9e41cc7859b458
irfanpule <irfan.pule2@gmail.com>
2020-03-02 23:19:40 +0700

    refactor maas request class

--- hujan_ui/maas/utils.py
+++ hujan_ui/maas/utils.py
@@ -3,6 +3,33 @@ from django.conf import settings
 from requests_oauthlib import OAuth1Session, OAuth1
 
 
+class MAAS:
+
+    def __init__(self):
+        self.url = settings.MAAS_URL + "api/2.0/"
+
+    def maas_connect(self):
+        consumer_key, token_key, token_secret = settings.MAAS_API_KEY.split(":")
+        return OAuth1Session(
+            consumer_key,
+            client_secret='',
+            resource_owner_key=token_key,
+            resource_owner_secret=token_secret,
+            signature_method='PLAINTEXT',
+            signature_type='auth_header'
+        )
+
+    def get(self, uri, params=None, headers=None):
+        maas = self.maas_connect()
+        default_headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        }
+        if headers:
+            default_headers.update(headers)
+
+        return maas.get(self.url + uri, headers=default_headers)
+
 
 def maas_connect():
     consumer_key, token_key, token_secret = settings.MAAS_API_KEY.split(":")
--- hujan_ui/maas/views.py
+++ hujan_ui/maas/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from hujan_ui.maas.utils import maas_connect
+from hujan_ui.maas.utils import MAAS
 
 
 @login_required
@@ -14,10 +14,8 @@ def machines(request):
         with open(settings.DIR_EX_RESPONSE + "machines.json") as readfile:
             machines = json.load(readfile)
     else:
-        auth = maas_connect()
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        res = requests.get(settings.MAAS_URL + "api/2.0/machines/", headers=headers, auth=auth)
-        machines = res.json()
+        maas = MAAS()
+        machines = maas.get("machines/").json()
 
     context = {
         'title': 'Machines',
@@ -33,10 +31,8 @@ def machine_details(request, system_id):
         with open(settings.DIR_EX_RESPONSE + "machine_details.json") as readfile:
             machine = json.load(readfile)
     else:
-        auth = maas_connect()
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        res = requests.get(settings.MAAS_URL + f"api/2.0/machines/{system_id}/", headers=headers, auth=auth)
-        machine = res.json()
+        maas = MAAS()
+        machine = maas.get(f"machines/{system_id}/").json()
 
     context = {
         'title': f"Machines - {machine['fqdn']}",
