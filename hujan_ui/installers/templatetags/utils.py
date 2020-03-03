14bd39abd9e891fc23b7dad555266cf3cfdd9739
irfanpule <irfan.pule2@gmail.com>
2020-03-03 11:04:38 +0700

    machine detail storage

--- hujan_ui/installers/templatetags/utils.py
+++ hujan_ui/installers/templatetags/utils.py
@@ -19,10 +19,18 @@ def sizify(value, suffix=""):
     else:
         value = value / 1073741824.0
         # ext = 'gb'
-    
+
     if suffix:
         return '%s %s' % (str(round(value, 2)), suffix)
     return str(round(value, 2))
 
 
+def bool_icon(value):
+    if value:
+        return "<i class='fa fa-check-circle text-success'></i>"
+    else:
+        return "<i class='fa fa-times-circle text-danger'></i>"
+
+
 register.filter('sizify', sizify)
+register.filter('bool_icon', bool_icon)
