df39cb1890c6a4f79e540b0d217ca69665128012
alinnural <alin@btech.co.id>
2020-04-06 21:44:21 +0700

    edit dan delete server

--- hujan_ui/installers/servers/forms.py
+++ hujan_ui/installers/servers/forms.py
@@ -4,19 +4,26 @@ from hujan_ui import maas
 from hujan_ui.installers.models import Server
 
 
-class AddServerForm(forms.Form):
+class AddServerForm(forms.ModelForm):
     machine = forms.ChoiceField(label='Name')
-    ip_address = forms.CharField(widget=forms.Select)
-    description = forms.CharField(required=False, help_text="Short description from this server")
-    system_id = forms.CharField()
-    name = forms.CharField()
+
+    class Meta:
+        model = Server
+        fields = ['machine','ip_address', 'description', 'system_id', 'name']
+        widgets = {
+            'ip_address': forms.Select(),
+            'description': forms.TextInput(attrs={'required': False}),
+            'system_id': forms.HiddenInput(),
+            'name': forms.HiddenInput()
+        }
+        help_texts = {
+            'description': 'Short description from this server',
+        }
 
     def __init__(self, *args, **kwargs):
         self.machines = self.get_choices_machines()
         super().__init__(*args, **kwargs)
         self.fields['machine'].choices = self.machines
-        self.fields['system_id'].widget = forms.HiddenInput()
-        self.fields['name'].widget = forms.HiddenInput()
 
     def get_choices_machines(self):
         resp = maas.get_machines()
@@ -32,3 +39,20 @@ class AddServerForm(forms.Form):
             description=clean_data['description'],
             system_id=clean_data['system_id']
         )
+
+
+class EditServerForm(AddServerForm):
+    def __init__(self, *args, **kwargs):
+        self.id = kwargs['instance'].id
+        super().__init__(*args, **kwargs)
+        self.fields['machine'].initial = kwargs['instance'].system_id
+        
+    def save(self):
+        clean_data = super().clean()
+        server = Server.objects.get(id=self.id)
+        server.name = clean_data['name']
+        server.ip_address = clean_data['ip_address']
+        server.description = clean_data['description']
+        server.system_id = clean_data['system_id']
+        return server.save()
+        
--- hujan_ui/installers/servers/views.py
+++ hujan_ui/installers/servers/views.py
@@ -3,11 +3,11 @@ import json
 import sweetify
 
 from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from hujan_ui.installers.models import Server
-from .forms import AddServerForm
+from .forms import AddServerForm, EditServerForm
 
 
 @login_required
@@ -39,3 +39,31 @@ def add(request):
         'col_size': '12',
     }
     return render(request, "installers/add-server.html", context)
+
+
+@login_required
+def edit(request, id):
+    server = get_object_or_404(Server, id=id)
+    form = EditServerForm(data=request.POST or None, instance=server)
+    if form.is_valid():
+        form.save()
+        sweetify.success(request, _(f"Successfully edited server"), button='OK', icon='success')
+        return redirect("installer:servers:index")
+
+    context = {
+        'title': 'Edit Server',
+        'form': form,
+        'menu_active': 'servers',
+        'title_submit': 'Edit Server',
+        'col_size': '12',
+        'old_ip_address': server.ip_address
+    }
+    return render(request, "installers/add-server.html", context)
+
+
+@login_required
+def delete(request, id):
+    server = get_object_or_404(Server, id=id)
+    server.delete()
+    sweetify.success(request, _(f"Successfully deleted server"), icon='success', button='OK')
+    return redirect("installer:servers:index")
