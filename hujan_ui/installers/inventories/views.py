b1d0b53cd29cfa3e7854215a6117d3d0b323ae35
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-04-28 18:50:27 +0700

    multinode writer

--- hujan_ui/installers/inventories/views.py
+++ hujan_ui/installers/inventories/views.py
@@ -1,13 +1,11 @@
-import requests
-import json
 import sweetify
-
-from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.translation import ugettext_lazy as _
 
 from hujan_ui.installers.models import Inventory
 from .forms import InventoryForm
+from .utils import save_inventory_multi_node
 
 
 @login_required
@@ -28,6 +26,7 @@ def add(request):
 
     if form.is_valid():
         form.save()
+        save_inventory_multi_node()
         sweetify.success(request, _(f"Successfully added inventory"), button='OK', icon='success')
         return redirect("installer:inventories:index")
 
@@ -47,6 +46,7 @@ def edit(request, id):
     form = InventoryForm(data=request.POST or None, instance=inventory)
     if form.is_valid():
         form.save()
+        save_inventory_multi_node()
         sweetify.success(request, _(f"Successfully edited inventory"), button='OK', icon='success')
         return redirect("installer:inventories:index")
 
@@ -64,5 +64,6 @@ def edit(request, id):
 def delete(request, id):
     inventory = get_object_or_404(Inventory, id=id)
     inventory.delete()
+    save_inventory_multi_node()
     sweetify.success(request, _(f"Successfully deleted inventory"), icon='success', button='OK')
     return redirect("installer:inventories:index")
