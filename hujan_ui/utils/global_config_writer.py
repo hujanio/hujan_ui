52c934dd4ae69c7c4dd099135a4581576ff94cd5
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-05-19 19:40:15 +0700

    global config writer

---
+++ hujan_ui/utils/global_config_writer.py
@@ -0,0 +1,48 @@
+class GlobalConfigWriter:
+    file_name = "/etc/kolla/globals.yml"
+
+    def __init__(self):
+        self.entry = {}
+
+    def add_entry(self, key: str, value):
+        entry_value = value
+        if type(value) == bool:
+            entry_value = "yes" if value else "no"
+
+        self.entry[key] = entry_value
+
+    def set_entry(self, entry):
+        for k, v in entry.items():
+            self.add_entry(k, v)
+
+    def save(self):
+        with open(self.file_name, 'w') as f:
+            for k, v in self.entry.items():
+                if type(v) == int:
+                    f.write("%s: %s\n" % (k, v))
+                else:
+                    f.write("%s: \"%s\"\n" % (k, v))
+
+    @staticmethod
+    def save_from_model(global_config):
+        writer = GlobalConfigWriter()
+        writer.set_entry({
+            "kolla_install_type": global_config.installation_type,
+            "openstack_release": global_config.openstack_release,
+            "kolla_internal_vip_address": global_config.internal_vip_address,
+            "kolla_external_vip_address": global_config.external_vip_address,
+            "kolla_enable_tls_external": global_config.enable_tls_on_external_api,
+            "enable_ceph": global_config.enable_ceph_service,
+            "enable_cinder": global_config.enable_cinder_service,
+            "enable_magnum": global_config.enable_magnum_service,
+            "enable_neutron_fwaas": global_config.enable_fwaas,
+            "enable_neutron_qos": global_config.enable_qos,
+            "enable_neutron_agent_ha": global_config.enable_ha_agent,
+            "enable_neutron_vpnaas": global_config.enable_vpnaas,
+            "enable_octavia": global_config.enable_octavia_service,
+            "ceph_pool_pg_num": global_config.ceph_pool_pg_num,
+            "ceph_pool_pgp_num": global_config.ceph_pool_pgp_num,
+            "glance_backend_ceph": global_config.glance_backend_using_ceph,
+            "glance_backend_file": global_config.glance_backend_file
+        })
+        writer.save()
