945bac4cc947b90171b44c289fccb3c7d5081e30
Muhamad Anjar <arvanria@gmail.com>
2020-11-26 11:52:29 +0700

    bug data fabric #41

--- hujan_ui/maas/subnets/views.py
+++ hujan_ui/maas/subnets/views.py
@@ -1,4 +1,3 @@
-import json
 import sweetify
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import render, redirect
@@ -8,6 +7,7 @@ from django.http import JsonResponse
 from hujan_ui.maas.utils import MAAS
 from hujan_ui import maas
 from .forms import SubnetForm, SubnetAddForm
+from hujan_ui.maas.exceptions import MAASError
 
 
 @login_required
@@ -16,11 +16,11 @@ def index(request):
         return JsonResponse({'subnets': maas.get_subnets()})
 
     context = {
-        'title': 'Subnets',
+        'title': 'Subnet By Fabric',
         'subnets': maas.get_subnets(),
         'fabrics': maas.get_fabrics(),
         'spaces': maas.get_spaces(),
-        'group_subnet': maas.get_subnet_infabric(),
+        'group_subnet': maas.get_subnet_byfabric(),
         'menus_active': 'subnets_active'
     }
     return render(request, 'maas/subnets/index.html', context)
@@ -28,12 +28,12 @@ def index(request):
 
 @login_required
 def detail(request, subnet_id):
-    
+
     subnet = maas.get_subnets(subnet_id)
     # unr = maas.get_subnets(subnet_id,op='unreserved_ip_ranges')
     # stat = maas.get_subnets(subnet_id,op='statistics')
     # TODO untuk data unreserved dan statistic masih belum sesuai antara api dan maas yang ada 
-    rir = maas.get_subnets(subnet_id,op='reserved_ip_ranges')
+    rir = maas.get_subnets(subnet_id, op='reserved_ip_ranges')
 
     if request.is_ajax():
         return JsonResponse({'subnet': subnet})
@@ -53,14 +53,17 @@ def detail(request, subnet_id):
 @login_required
 def add(request):
     form = SubnetAddForm(request.POST or None)
-    if form.is_valid():
-        m = MAAS()
-        data = form.clean()
-        resp = m.post('subnets/', data=data)
-        if resp.status_code in m.ok:
-            sweetify.success(request, _('Subnet Added Successfully'), timer=2000)
-            return redirect('maas:subnets:index')
-        sweetify.warning(request, _(resp.text), timer=5000)
+    if request.method == 'POST' and form.is_valid():
+        try:
+            m = MAAS()
+            data = form.clean()
+            resp = m.post('subnets/', data=data)
+            if resp.status_code in m.ok:
+                sweetify.success(request, _('Subnet Added Successfully'), icon='success', timer=2000)
+                return redirect('maas:subnets:index')
+            sweetify.warning(request, _(resp.text), timer=5000)
+        except (MAASError) as e:
+            sweetify.error(request, str(e), button='OK', icon='error', timer=5000)
     context = {
         'title': 'Form Add Subnet',
         'form': form
@@ -79,15 +82,15 @@ def edit(request, subnet_id):
         data = form.clean()
         if data['vlan']:
             vl = maas.get_vlans(int(data['vlan']))
-            data['vid'] =  vl['vid']
-            data['fabric'] = vl['fabric_id'] 
-        
+            data['vid'] = vl['vid']
+            data['fabric'] = vl['fabric_id']
+
         resp = m.put(f'subnets/{subnet_id}/', data=data)
         if resp.status_code in m.ok:
             sweetify.success(request, _('Subnet Update Successfully'), timer=2000)
             return redirect('maas:subnets:index')
         sweetify.warning(request, _(resp.text), timer=5000)
-    
+
     context = {
         'title': 'Form Edit Subnet',
         'form': form
@@ -99,15 +102,12 @@ def edit(request, subnet_id):
 def delete(request, subnet_id):
     subnets = maas.get_subnets()
     subnet = [s for s in subnets if s['id'] == subnet_id]
-    if subnet[0] != None:
+    if subnet[0] is not None:
         m = MAAS()
         resp = m.delete(f'subnets/{subnet_id}/')
         if resp.status_code in m.ok:
             sweetify.success(request, _('Subnet Deleted Successfully'), timer=2000)
             return redirect('maas:subnets:index')
         sweetify.warning(request, _(resp.text), timer=5000)
-    
+
     return redirect('maas:subnets:index')
-
-
-    
