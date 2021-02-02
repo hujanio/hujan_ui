84f93c2e98e3ba3722ae0ab2c3890cefa7d0fa2a
Muhamad Anjar <arvanria@gmail.com>
2021-02-02 19:30:56 +0700

    delete check table

--- hujan_ui/maas/utils.py
+++ hujan_ui/maas/utils.py
@@ -84,16 +84,3 @@ class MAAS:
     def _validate_request(self, resp):
         if resp.status_code in self.fail:
             raise MAASError(resp.text)
-
-    def db_table_exists(table, cursor=None):
-        try:
-            if not cursor:
-                from django.db import connection
-                cursor = connection.cursor()
-            if not cursor:
-                raise Exception
-            table_names = connection.introspection.get_table_list(cursor)
-        except:
-            raise Exception("unable to determine if the table '%s' exists" % table)
-        else:
-            return table in table_names
